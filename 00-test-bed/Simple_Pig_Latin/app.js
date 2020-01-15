5dc5040afe075f8adea81142296999a8ddfd0961
unknown <diego.maquill@gmail.com>
2020-01-14 19:10:15 -0500

    update to pig_latin

--- 00-test-bed/Simple_Pig_Latin/app.js
+++ 00-test-bed/Simple_Pig_Latin/app.js
@@ -4,11 +4,12 @@ then add "ay" to the end of the word.Leave punctuation
 marks untouched.
 */
 //Code here
-
+/* 
 function pigIt(str) {
     return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
 }
-/* 
+ */
+
 var notAllow = /[?!]/g;
 
 function pigIt(str) {
@@ -18,13 +19,10 @@ function pigIt(str) {
             word = word + "";
         } else {
             word = word.slice(1) + word.charAt(0) + "ay";
-        } /* else {
-            word;
-        } 
+        }
         return word;
-
     }).join(' ');
-} */
+}
 console.log(pigIt('Pig latin is cool ?'));
 console.log(pigIt('Pig latin is cool !'));
 console.log(pigIt('Pig latin is cool?'));
